f36415b72b4a42e06034d76711857e2f6f355d89
mahdi <mahdideveloper80@gmail.com>
2023-11-13 21:30:12 +0330

    upgrade js and html file in folder media

--- public/panel_admin/media/index.js
+++ public/panel_admin/media/index.js
@@ -1,8 +1,85 @@
 let SelectFolder = document.querySelector('#SelectFolder');
 let input_search_media = document.querySelector('.input_search_media');
 let container_medias_gallery = document.querySelector('.container_medias_gallery');
+let exit_panel = document.querySelector('#exit_panel');
+let title_body_left_panel = document.querySelector('.title_body_left_panel');
+let profile_user = document.querySelector('#profile_user');
+let InfoToken = localStorage.getItem('token');
 let token = localStorage.getItem('token');
+let title_input_clock = document.querySelector('.title_input_clock');
+let userInfo = JSON.parse(localStorage.getItem('user'));
 let url = 'http://localhost:3000/';
+exit_panel.addEventListener('click', () => {
+    Swal.fire({
+        title: 'آیا از خروج مطمئن هستی؟',
+        text: "با کلیک روی خروج از حساب کاربریت خارج میشی",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'منصرف شدن',
+        confirmButtonText: 'خروج از حساب کاربری'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.removeItem('user');
+            location.reload();
+        }
+    })
+});
+
+function isLogin(pathRedirect) {
+    if (userInfo) {
+        fetch(`${url}api/public/users/all/`)
+            .then(res => res.json())
+            .then(go => {
+                let isLoginUser = go.some(user => {
+                    return user.username == userInfo.username && user.password == userInfo.password && userInfo.type == user.type
+                });
+                fetch(`${url}api/token/isVriefy/${token}/`)
+                    .then(res => res.text())
+                    .then(go => {
+                        if (!isLoginUser || go !== 'Access Token') {
+                            localStorage.removeItem('user');
+                            localStorage.removeItem('token');
+                            location.reload();
+                        }
+                    })
+            })
+    } else {
+        location.href = pathRedirect;
+    }
+}
+
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-left",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
+function GetProfileUser(email) {
+    fetch(`${url}api/public/users/profile/${email}/`, {
+        method: 'GET',
+        headers: {
+            'Authorization': InfoToken
+        }
+    })
+        .then(res => res.text())
+        .then(res => {
+            profile_user.src = res
+        })
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     container_medias_gallery.innerHTML = '';
@@ -12,6 +89,9 @@ window.addEventListener('DOMContentLoaded', () => {
         <i class="bx bxs-info-circle"></i>
         </div>
                 `);
+    isLogin('../../auth/index.html');
+    GetProfileUser(userInfo.email);
+    title_body_left_panel.innerHTML = `${userInfo.name == 'empty' ? 'مهمان' : userInfo.name} عزیز؛ خوش اومدی 🙌`
 });
 
 SelectFolder.addEventListener('change', (e) => {
