7637dddc3f0197b3352f23a5d21700d25ea461b6
mahdi <mahdideveloper80@gmail.com>
2023-11-13 21:16:12 +0330

    create api search media

--- backend/admin/media.js
+++ backend/admin/media.js
@@ -12,4 +12,18 @@ mediaRouterAdmin.get(`/admin/media/:folder/`, (req, res) => {
     });
 });
 
+mediaRouterAdmin.get(`/admin/media/search/:folder/:txt/`, (req, res) => {
+    fs.readdir(`./uploads/${req.params.folder}`, (err, result) => {
+        if (err) {
+            console.log('Error Get All Media', err);
+        } else {
+            let resultSearch = result.filter(file => {
+                const regex = new RegExp(req.params.txt, 'i');
+                return regex.test(file);
+            });
+            res.json(resultSearch);
+        }
+    });
+});
+
 module.exports = mediaRouterAdmin
